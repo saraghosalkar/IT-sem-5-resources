7070f3f3a029eed1800a40289a434937560f741f
Sara <sararghosalkar@gmail.com>
2022-10-30 15:45:15 +0530

    Generators

---
+++ Internet Programming/Coding Practice/PracsPract/Ass4_JS/generators.js
@@ -0,0 +1,10 @@
+function* gen(){
+    yield 100;
+    yield;
+    yield 200;
+}
+var mygen=gen();
+console.log(mygen.next().value);
+console.log(mygen.next().value);
+console.log(mygen.next().value);
+console.log(mygen.next().value);
