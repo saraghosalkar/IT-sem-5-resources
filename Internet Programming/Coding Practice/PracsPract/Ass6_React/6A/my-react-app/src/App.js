e81fffc154cad6a6d4fd417122de031ff8c42590
Sara <sararghosalkar@gmail.com>
2022-10-30 18:36:04 +0530

    React

---
+++ Internet Programming/Coding Practice/PracsPract/Ass6_React/6A/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome name="Sara" />
+      <Welcome name="Kavya" />
+      <Welcome name="Manisha" />
+    </div>
+  )
+}
+export default App;
